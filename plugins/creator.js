eb04cf1763ad0e5e56dc927e9b5bdfc918b58152
Amal-dx <88432886+amal-dx@users.noreply.github.com>
2021-11-02 12:49:09 +0530

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,7 @@
 function handler(m) {
   // Ini cuma contoh, jgn di uncomment -_-
   // F this.sendContact(m.chat, '62815158600891', 'Nurutomo', m)
-  this.sendContact(m.chat, '919002370263', 'JJ-Oreki', m)
+  this.sendContact(m.chat, '917736308760', 'Amal-sir', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
