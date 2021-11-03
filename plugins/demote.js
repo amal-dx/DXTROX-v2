26ecd95d8491b4cccef46ac63887a66ae8762af6
Amal-dx <88432886+amal-dx@users.noreply.github.com>
2021-11-03 18:47:15 +0530

    Update demote.js

--- plugins/demote.js
+++ plugins/demote.js
@@ -1,3 +1,6 @@
+/*
+   Coded by amal-dx
+*/
 let handler = async (m, { conn, participants }) => {
   let members = participants.filter(member => member.isAdmin).map(member => member.jid)
   let users = m.mentionedJid.filter(user => members.includes(user))
