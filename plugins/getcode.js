28b4908a3cbbcab6fc0eeced533ce7c265d73902
Amal-dx <88432886+amal-dx@users.noreply.github.com>
2021-11-03 19:05:58 +0530

    Update getcode.js

--- plugins/getcode.js
+++ plugins/getcode.js
@@ -1,3 +1,6 @@
+/*
+   Coded by amal-dx
+*/
 let handler = async (m, { conn, usedPrefix }) => {
   if (global.conn.user.jid == conn.user.jid) conn.reply(m.chat, 'This command is only for bots', m)
   else global.conn.reply(conn.user.jid, `${usedPrefix}jadibot ${Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())).toString('base64')}`, m)
