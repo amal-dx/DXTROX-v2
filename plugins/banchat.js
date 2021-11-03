5fd954fdcffc18bf20522ae992a5c19eb50948de
Amal-dx <88432886+amal-dx@users.noreply.github.com>
2021-11-03 15:01:53 +0530

    Update banchat.js

--- plugins/banchat.js
+++ plugins/banchat.js
@@ -1,3 +1,6 @@
+/*
+   Coded by amal
+*/
 let handler = async (m, { conn, participants }) => {
   // if (participants.map(v=>v.jid).includes(global.conn.user.jid)) {
     global.DATABASE._data.chats[m.chat].isBanned = true
