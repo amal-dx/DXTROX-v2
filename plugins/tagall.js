fc302a6fa1697957d7c35640c21f65148ad46161
Amal-dx <88432886+amal-dx@users.noreply.github.com>
2021-11-03 22:31:25 +0530

    Update tagall.js

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -1,3 +1,6 @@
+/*
+   Coded by amal-dx
+*/
 let handler = async (m, { conn, text, participants }) => {
   let users = participants.map(u => u.jid)
   m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
