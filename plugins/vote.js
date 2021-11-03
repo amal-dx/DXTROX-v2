df78e2ef6020d5b64f1b3671bf7437a1dfabf82d
Amal-dx <88432886+amal-dx@users.noreply.github.com>
2021-11-03 11:09:03 +0530

    Update vote.js

--- plugins/vote.js
+++ plugins/vote.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const voting = JSON.parse(fs.readFileSync('./lib/voting.json'))
+const voting = JSON.parse(fs.readFileSync('./plugins/voting.json'))
 
 const sleep = async (ms) => {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -7,16 +7,16 @@ const sleep = async (ms) => {
 
 const delVote = (_id) => {
 	voting.splice(_id,0)
-    fs.writeFileSync(`./lib/voting.json`, JSON.stringify(voting))
-    fs.unlinkSync(`./lib/vote/${_id}.json`)
-    fs.unlinkSync(`./lib/${_id}.json`)
+    fs.writeFileSync(`./plugins/voting.json`, JSON.stringify(voting))
+    fs.unlinkSync(`./plugins/vote/${_id}.json`)
+    fs.unlinkSync(`./plugins/${_id}.json`)
 }
 
 const addVote = async(_id,_value1,_value2,_value3,reply) => {
 	voting.push(_id)
-    fs.writeFileSync(`./lib/${_id}.json`,'[]')
-    fs.writeFileSync(`./lib/vote/${_id}.json`,'[]')
-    fs.writeFileSync('./lib/voting.json', JSON.stringify(voting)) 
+    fs.writeFileSync(`./plugins/${_id}.json`,'[]')
+    fs.writeFileSync(`./plugins/vote/${_id}.json`,'[]')
+    fs.writeFileSync('./plugins/voting.json', JSON.stringify(voting)) 
     await sleep(2000)
     let votes = JSON.parse(fs.readFileSync(`./lib/vote/${_id}.json`))  
     votes.push({
@@ -24,15 +24,15 @@ const addVote = async(_id,_value1,_value2,_value3,reply) => {
     votes: _value2.trim() +'@s.whatsapp.net',
     durasi: _value3 
     })  
-    fs.writeFileSync(`./lib/vote/${_id}.json`, JSON.stringify(votes)) 
+    fs.writeFileSync(`./plugins/vote/${_id}.json`, JSON.stringify(votes)) 
     setTimeout(async function() {
 	let vote = JSON.parse(fs.readFileSync(`./lib/${_id}.json`))
     let tru = vote.filter(a => a.voting == '✅')
     let fals = vote.filter(a => a.voting == '❌')
     reply(`*Waktu Habis*\n\n*Hasil Akhir*\n✅ = ${tru.length}\n❌ = ${fals.length}`)
-    fs.unlinkSync(`./lib/vote/${_id}.json`)
-    fs.unlinkSync(`./lib/${_id}.json`)
-    fs.writeFileSync(`./lib/voting.json`, JSON.stringify(voting)) 
+    fs.unlinkSync(`./plugins/vote/${_id}.json`)
+    fs.unlinkSync(`./plugins/${_id}.json`)
+    fs.writeFileSync(`./plugins/voting.json`, JSON.stringify(voting)) 
     }, _value3 * 60 * 1000);
 }
 
