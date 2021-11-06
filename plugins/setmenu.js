6e73d26d68389d49827392c689d0a5fe1644180a
Amal-dx <88432886+amal-dx@users.noreply.github.com>
2021-11-06 14:46:18 +0530

    Update setmenu.js

--- plugins/setmenu.js
+++ plugins/setmenu.js
@@ -1,28 +1,25 @@
-/*
-   Coded by amal-dx
-*/
-let handler  = async (m, { conn, command, text }) => {
+let handler = async (m, { conn, command, text }) => {
   let type = command.replace(/^set(menu|help|\?)/, '').toLowerCase()
   if (type == '') {
     if (text) {
       conn.menu = text
-      conn.reply(m.chat, 'Menu set successfully\n' + info, m)
+      conn.reply(m.chat, 'Menu berhasil diatur\n' + info, m)
     } else {
       conn.menu = {}
-      conn.reply(m.chat, 'Menu is reset', m)
+      conn.reply(m.chat, 'Menu direset', m)
     }
   } else {
     conn.menu = typeof conn.menu == 'object' ? conn.menu : {}
     if (text) {
       conn.menu[type] = text
-      conn.reply(m.chat, 'Menu ' + type + ' set up successfully\n' + info, m)
+      conn.reply(m.chat, 'Menu ' + type + ' berhasil diatur\n' + info, m)
     } else {
       delete conn.menu[type]
-      conn.reply(m.chat, 'Menu ' + type + ' reset', m)
+      conn.reply(m.chat, 'Menu ' + type + ' direset', m)
     }
   }
 }
-handler.help = ['', 'before','header','body','footer','after'].map(v => 'setmenu' + v + ' <Text>')
+handler.help = ['', 'before', 'header', 'body', 'footer', 'after'].map(v => 'setmenu' + v + ' <teks>')
 handler.tags = ['owner']
 handler.command = /^set(menu|help|\?)(before|header|body|footer|after)?$/i
 handler.owner = true
@@ -47,6 +44,8 @@ $maxexp (Exp To Level Up)
 %totalexp (Total Exp)
 %xp4levelup (Exp yang dibutuhkan untuk levelup)
 %limit (Limit)
+%level (level)
+%role (Role)
 %name (Nama)
 %weton (Weton Hari ini)
 %week (Hari)
@@ -66,4 +65,5 @@ Bagian Menu Header & Footer:
 Bagian Menu Body:
 %cmd (Command)
 %islimit (Jika command di limit)
+%isPremium (Jika command premium)
 `.trim()
